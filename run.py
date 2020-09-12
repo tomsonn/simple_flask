de4749c6cbf94b188ce99ca2b7957920e2c7e20c
Tomas Macko <tomas.macko@kiwi.com>
2020-09-12 08:31:32 +0200

    Fixed css file, buttons don't show style-box after activation

--- run.py
+++ run.py
@@ -18,16 +18,6 @@ def home():
 def user(user):
 	return render_template('user.html', user=user)
 
-@app.route('/minigames/stopwatch', methods=['POST', 'GET'])
-def stopwatch():
-	generated_number = 0
-	if 'sw_start_btn' in request.form:
-		generated_number = get_random_number()
-	elif 'sw_stop_btn' in request.form:
-		generated_number = get_random_number() + 10.0
-
-	return render_template('stopwatch.html', generated_number=generated_number)
-
 @app.route('/login')
 def login():
 	return render_template('login.html')
@@ -35,6 +25,14 @@ def login():
 @app.route('/logout')
 def logout():
 	return render_template('logout.html')
+
+@app.route('/minigames/stopwatch')
+def stopwatch():
+	return render_template('stopwatch.html')
+
+@app.route('/minigames/cheap-food', methods=['POST', 'GET'])
+def cheap_food():
+	return render_template('cheap_food.html')
 
 
 if __name__ == '__main__':
