ae8b5ca4a78ba011d8283bb3f8dec38cccce7ba0
Tomas Macko <tomas.macko@kiwi.com>
2020-09-21 16:46:51 +0200

    Scraped subcategories from kupi.cz

--- classes/kupi_scrapper.py
+++ classes/kupi_scrapper.py
@@ -18,6 +18,10 @@ class KupiScrapper:
 		self.ENDPOINT_LEAFLETS = '/letaky/hypermarkety-a-supermarkety'
 
 	def get_html_from_url(self, url):
+		"""
+			Returns HTML file from desired endpoint as a string
+		"""
+
 		try:
 			response = requests.Session().get(url)
 			response.raise_for_status()
@@ -26,6 +30,15 @@ class KupiScrapper:
 			print(f'Couldn\'t get HTML content of {url} endpoint.')
 
 	def scrape_categories(self):
+		"""
+			Scrape categories from `slevy` endpoint.
+			Return dictionary of categories in following format:
+			{
+				name_of_the_category [str] -> decoded name 	  :	`endpoint` [str] -> endpoint to
+										  of the category 						discounts in desired shops																	
+			}
+		"""
+
 		URL_CATEGORIES = self.URL_BASE + self.ENDPOINT_DISCOUNTS
 		categories_html = self.get_html_from_url(URL_CATEGORIES)
 
@@ -44,9 +57,20 @@ class KupiScrapper:
 		# URL endpoint as the value
 		categories_dict = {key.text.strip(): value.get('href') for key, value in zip(category_el_key, category_el_value)}
 
+		print(json.dumps(categories_dict, indent=4))
 		return categories_dict
 
 	def scrape_shops(self):
+		"""
+			Scrape shops from `hypermarkety-a-supermarkety` endpoint.
+			It's only including shops which sell food or drinks.
+			Returns dictionary in following format:
+			{
+				`name_of_the_shop` [str] -> decoded name : `endpoint` [str] -> endpoint to discounts
+										    of the shop 					   in desired shops.
+			}
+		"""
+
 		URL_SHOPS = self.URL_BASE + self.ENDPOINT_LEAFLETS
 		shops_html = self.get_html_from_url(URL_SHOPS)
 
@@ -65,11 +89,46 @@ class KupiScrapper:
 
 		return shops_dict
 
-	def scrape_items_by_categories_and_shops(self, categories, shops):
-		categories_endpoints_list = list(categories.values())
+	def scrape_subcategories(self, categories):
+		"""
+			Scrape subcategories of each category
+			Returns dictionary with categories and each category
+			has list of other dictionaries mentioned below as its value.
+			Dictionaries in list as value of each category is in following format: 
+			{
+				'name': name [str] -> Name in decoded format
+				'data-category': raw_name [str] -> Name in raw format
+				'endpoint': href [str] -> endpoint to discounts in desired subcategory
+			}
+		"""
+
+		URL_SUBCATEGORIES = self.URL_BASE + list(categories.values())[0]
+		subcategories_html = self.get_html_from_url(URL_SUBCATEGORIES)
+
+		try:
+			soup = BeautifulSoup(subcategories_html, 'html.parser')
+		except: 
+			print('Couldn\'t scrape kupi.cz HTML to get subcategories.')
+
+		subcategories_dict = {}
+		# Find all `unordered_list` elements with datatype: `subcategory`
+		subcategories_el_parent = soup.find_all('ul', {'role': 'menu', 'data-type': 'subcategory'})
+		for subcategory_el_parent in subcategories_el_parent:
+			category = subcategory_el_parent.get('data-category')
+			# Add key to dictionary with all of the values described above
+			subcategories_dict[category] = [{'name': sc_name.a.text,
+											 'data-category': sc_name.get('data-category'), 
+											 'endpoint': sc_name.a.get('href')}
+											   for sc_name in subcategory_el_parent.findChildren('li')
+											   if sc_name.get('data-category') != 'vse']
+		
+		return subcategories_dict
+
+	### TODO
+	def scrape_items_by_subcategory_from_shop(self, subcategory_endpoint, shops):
 		shops_endpoints_list = [shop_endpoint.split('/')[-1] for shop_endpoint in shops.values()]
 
-		URL_ITEMS = f'{self.URL_BASE}{categories_endpoints_list[0]}/{shops_endpoints_list[0]}?page=40'
+		URL_ITEMS = f'{self.URL_BASE}{subcategory_endpoint}/{shops_endpoints_list[0]}'
 		items_html = self.get_html_from_url(URL_ITEMS)
 		try:
 			soup = BeautifulSoup(items_html, 'html.parser')
@@ -104,7 +163,8 @@ def main():
 	kupi_scrapper = KupiScrapper()
 	categories_dict = kupi_scrapper.scrape_categories()
 	shops_dict = kupi_scrapper.scrape_shops()
-	kupi_scrapper.scrape_items_by_categories_and_shops(categories_dict, shops_dict)
+	# kupi_scrapper.scrape_items_by_categories_and_shops(categories_dict, shops_dict)
+	# kupi_scrapper.scrape_subcategories(categories_dict)
 
 if __name__ == '__main__':
 	main()
